15239e49a1a6ea9fb7cadee7e75fad089b123d1f
guptadhairy <21it3032@rgipt.ac.in>
2023-08-08 00:40:09 +0530

    Just for testing

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,17 +1,30 @@
-import { Contact } from "../models/contact.js";
-
+// import { Contact } from "../models/contact.js";
+import nodemailer from 'nodemailer'
 
 export const contactUs = async (req, res) => {
-    try {
-        const { name, email, message } = req.body;
-
-    await Contact.create({ name, email, message })
-
-    res.status(200).json({
-        success: true,
-        reply: "Your message sent successfully !"
+    const { name, email, message } = req.body;
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail', // You can use other email providers as well
+        auth: {
+            user: process.env.USER, // Your email address
+            pass: process.env.PASS, // Your email password or application-specific password
+        },
     });
-}catch (error) {
-    console.log(error);
-}
-    } 
+    const mailOptions = {
+        from: process.env.USER, // Your email address
+        to: process.env.USER,
+        subject: name,
+        text: message,
+        replyTo: email,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully');
+        }
+    });
+} 
